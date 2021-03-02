3a2d0c2efdbc0b15e9f096d9a76656b89e6e0a7f
rhulk <quique1904@gmail.com>
2021-03-02 23:56:53 +0100

    ee

---
+++ Alquiler/src/main/java/com/bike/Alquiler/controller/Rest/BikeController.java
@@ -0,0 +1,31 @@
+package com.bike.Alquiler.controller.Rest;
+
+import java.util.Date;
+import java.util.LinkedList;
+import java.util.List;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+/*
+ * Necesidad en BBDD para cada registro a modo de historico
+ * 
+ * id_reserva, id_bike, id_persona, fecha_reserva
+ * 
+ */
+
+
+@RestController
+public class BikeController {
+	
+	List<Date> fechas = new LinkedList<Date>();
+	List<Integer> bicis = new LinkedList<Integer>();
+	
+	@GetMapping("/disponibles")
+	public List<Integer> greeting(@RequestParam(value = "fechas") List<Date> fechas) {
+		
+		return bicis;
+	}
+
+}
---
+++ Alquiler/src/main/java/com/bike/Alquiler/controller/Rest/ContactoController.java
@@ -0,0 +1,10 @@
+package com.bike.Alquiler.controller.Rest;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class ContactoController {
+	
+	
+
+}
