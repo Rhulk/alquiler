a514dd3ef659fcbbb8be1d4e2ccef43660a60103
kk <quique1904@gmail.com>
2021-03-02 13:17:03 +0100

    inicial

---
+++ Alquiler/src/test/java/com/bike/Alquiler/AlquilerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bike.Alquiler;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlquilerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
